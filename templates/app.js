df6d65db0446db596ded4445c9493e0fb535e2ba
Kelly Yuan <115099372+kellyyuan333@users.noreply.github.com>
2024-02-10 15:42:30 -0500

    Delete templates directory

--- templates/app.js
+++
@@ -1,5 +0,0 @@
-// JavaScript function to change header color on click
-function changeHeaderColor() {
-    var header = document.querySelector('header');
-    header.style.backgroundColor = '#00ff00'; // Change to a different color
-}
