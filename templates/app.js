995b0428be4f0488bef6662ba89aa7e46456dfcb
Kelly Yuan <115099372+kellyyuan333@users.noreply.github.com>
2024-02-10 13:44:19 -0500

    Add files via upload

---
+++ templates/app.js
@@ -0,0 +1,5 @@
+// JavaScript function to change header color on click
+function changeHeaderColor() {
+    var header = document.querySelector('header');
+    header.style.backgroundColor = '#00ff00'; // Change to a different color
+}
