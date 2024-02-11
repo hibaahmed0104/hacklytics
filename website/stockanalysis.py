80c131129058137d3d3377475df20486432f77ff
meghanamukkoti <136759257+meghanamukkoti@users.noreply.github.com>
2024-02-11 03:54:35 -0500

    Add files via upload

---
+++ website/stockanalysis.py
@@ -0,0 +1,21 @@
+import csv
+
+csv_filename = '/Users/meghanamukkoti/Desktop/hacklytics/Stoquette/website/stock_data.csv'
+output_csv_filename = '/Users/meghanamukkoti/Desktop/hacklytics/Stoquette/website/output_stock_data.csv'
+listName = ["AMZN", "ELV", "BAC", "C", "CMS", "CVS", "DUK", "FIS", "GM", "PGR", "HSY", "VRTX"]
+
+try:
+    with open(csv_filename, mode='r') as csvfile, open(output_csv_filename, mode='w', newline='') as output_csv:
+        reader = csv.reader(csvfile)
+        writer = csv.writer(output_csv)
+
+        for row in reader:
+            # Check if the first element is in the listName
+            if row[0] in listName:
+                value = row[4]
+                writer.writerow([value])
+
+except FileNotFoundError:
+    print(f"File '{csv_filename}' not found.")
+except Exception as e:
+    print(f"An error occurred: {e}")
